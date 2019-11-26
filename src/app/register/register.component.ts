f8293ee8b8f877a3f86de961366cb2b834a19f38
Ruben <lukesruben@gmail.com>
2019-11-26 15:51:25 +0100

    added register forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  name: string;
+  email: string;
+  password: string;
+  mobile: string;
+  age: number;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
