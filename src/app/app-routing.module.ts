efbfd89e9c70f72a6ef938e0fe287f7c91e08703
Ruben <lukesruben@gmail.com>
2019-11-26 13:26:35 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { QuestionsViewComponent } from "./questions-view/questions-view.component";
+import { QuestionsNewComponent } from "./questions-new/questions-new.component";
+import { QuestionsIndexComponent } from "./questions-index/questions-index.component";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "questions", component: QuestionsIndexComponent },
+  { path: "newquestion", component: QuestionsNewComponent },
+  { path: "viewquestion", component: QuestionsViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
